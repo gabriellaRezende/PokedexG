0191c878d8e43d344576dc9c3c7044433d74ccdc
gabriellaRezende <166410999+gabriellaRezende@users.noreply.github.com>
2025-03-31 16:55:43 +0100

    Atualiza dependências do React Native e renomeia HomeScreen para ContaScreen

--- src/screens/ContaScreen.tsx
+++ src/screens/ContaScreen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function HomeScreen() {
+export default function ContaScreen() {
   return (
     <View>
       <Text>Tela Conta</Text>
