24ccdb61ef2fa3cef4588c7a118f1f3964d09769
gabriellaRezende <166410999+gabriellaRezende@users.noreply.github.com>
2025-03-31 19:49:15 +0100

    Adiciona suporte a tipagem de navegação e atualiza tsconfig.json para incluir JSX do React Native

--- src/screens/PokedexScreen.tsx
+++ src/screens/PokedexScreen.tsx
@@ -1,10 +1,11 @@
-import { useNavigation } from "@react-navigation/native";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, Button } from "react-native";
+import { ParamList } from "../navegation/navegation"; // Importando o tipo ParamList
 
 
 export default function PokedexScreen() {
-    const navigation = useNavigation();
+    const navigation = useNavigation<NavigationProp<ParamList>>(); // Usando o tipo ParamList para tipar a navegação
 
   return (
     <View>
